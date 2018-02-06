4fec3a94666ba3684750e57b98569e15471374d1
Runner <841766635@qq.com>
2018-02-06 18:13:23 +0800

    字体文件及header

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import './css/common.less'
 import 'element-ui/lib/theme-chalk/index.css'
+import './assets/font/iconfont.css'
 
 
 Vue.config.productionTip = false
--- src/modules/layout/header.vue
+++ src/modules/layout/header.vue
@@ -1,6 +1,11 @@
 <template>
 	<header>
-header
+		<div class="layout-left">Vue Manage</div>
+		<div class="layout-right">
+			<i class="el-icon-bell"></i>
+			<i class="iconfont icon-skin"></i>
+		</div>
+
 	</header>
 </template>
 <script>
@@ -10,12 +15,28 @@ export default {
 }
 </script>
 
-<style type="less">
+<style lang="less" >
 header{
+	@leftWidth:240px;
 	width: 100%;
 	height: 60px;
 	background: #2e323d;
 	-webkit-box-sizing: border-box;
 	box-sizing: border-box;
+	.layout-left{
+		width:@leftWidth;
+		background:#1388ff;
+		color: #fff;
+		line-height: 60px;
+		font-size: 24px;
+		float: left;
+	}
+	.layout-right{
+		margin-left: @leftWidth;
+		i{
+			color:#fff;
+		}
+	}
 }
+
 </style>
