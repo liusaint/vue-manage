7d626fbe91d0e0963cfa2fed28cf55cce9e7fd5d
Runner <841766635@qq.com>
2018-02-06 17:37:33 +0800

    加入字体　加入less

--- src/modules/layout/header.vue
+++ src/modules/layout/header.vue
@@ -1,7 +1,7 @@
 <template>
-	<div>
+	<header>
 header
-	</div>
+	</header>
 </template>
 <script>
 
@@ -9,3 +9,13 @@ export default {
 	// name: 'header',
 }
 </script>
+
+<style type="less">
+header{
+	width: 100%;
+	height: 60px;
+	background: #2e323d;
+	-webkit-box-sizing: border-box;
+	box-sizing: border-box;
+}
+</style>
